197ccfefbbbf5711d7129eb005e3312eecb71603
Adam Lyskawa <a.lyskawa@gmail.com>
2013-07-03 14:52:21 +0200

    Version bump

--- content/NST.js
+++ content/NST.js
@@ -45,7 +45,7 @@
  * Namespaces
  */
 if (typeof ko.extensions === 'undefined') ko.extensions = {};
-if (typeof ko.extensions.NST === 'undefined') ko.extensions.NST = { version : '0.61' };
+if (typeof ko.extensions.NST === 'undefined') ko.extensions.NST = { version : '0.62' };
 
 /**
  * Regular expressions quoting (from phpjs.org) used for comments
